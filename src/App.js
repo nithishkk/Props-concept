53b50da88583c5abb3e362bf4f8a32742b37b3ff
nithishkk <109412506+nithishkk@users.noreply.github.com>
2023-02-28 16:23:44 +0530

    added props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from "react";
-import "./style.css";
-import Prodect from './Prodect'
+import React from 'react';
+import './style.css';
+import Prodect from './Prodect';
 
 export default function App() {
-  const prodect={
-    id:1,
-    name:"i phone",
-    price:120000,
-    dprice:123333
-  }
+  const prodect = {
+    id: 1,
+    name: 'i phone',
+    price: 120000,
+    dprice: 123333,
+  };
+  // const prodect1={
+  //   id:2,
+  //   name:"sumsung",
+  //   price:12,
+  //   dprice:12
+  // }
   return (
     <div>
-     
-      <Prodect id={prodect.id} name={prodect.name} price={prodect.price} dprice={prodect.dprice}/>
+      <Prodect {...prodect} />
     </div>
   );
 }
